3f7aa94634972ef33fd42af2e5cebb05cf03d21a
wanko <wanko@cs.uni-potsdam.de>
2021-04-01 09:55:17 +0200

    update create script and configs

--- create_all.sh
+++ create_all.sh
@@ -16,9 +16,13 @@ for config in $configs*.lp; do
             config_name="${config_name%.*}"
             scenario_name="${scenario_name%.*}"
             heuristic_name="${heuristic_name%.*}"
-            echo Creating "$config_name"_"$scenario_name"_"$heuristic_name"
-            clingo src/generator.lp src/arith.lp $config $scenario $heuristic --heuristic=Domain --out-atomf="%s." | tac | sed '7q;d' > $output/"$config_name"_"$scenario_name"_"$heuristic_name".lp    
-            python src/visualizer/visualizer.py --file $output/"$config_name"_"$scenario_name"_"$heuristic_name".lp --output $output/"$config_name"_"$scenario_name"_"$heuristic_name" 
+            if [ -s $output/"$config_name"_"$scenario_name"_"$heuristic_name".lp ]; then
+                echo "$config_name"_"$scenario_name"_"$heuristic_name" exists
+            else
+                echo Creating "$config_name"_"$scenario_name"_"$heuristic_name"
+                clingo src/generator.lp src/arith.lp $config $scenario $heuristic --heuristic=Domain --out-atomf="%s." | tac | sed '7q;d' > $output/"$config_name"_"$scenario_name"_"$heuristic_name".lp    
+                python src/visualizer/visualizer.py --file $output/"$config_name"_"$scenario_name"_"$heuristic_name".lp --output $output/"$config_name"_"$scenario_name"_"$heuristic_name" 
+            fi
         done
     done
 done
